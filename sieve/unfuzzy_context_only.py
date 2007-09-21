f44bb84d9ff85a23804aa0e0ea9eaa8e3141407d
Chusslove Illich <caslav.ilic@gmx.net>
2007-09-21 20:10:22 +0000

    Sieve to unfuzzy on context only change.
    svn path=/trunk/l10n-kde4/scripts/pology/; revision=715311

---
+++ sieve/unfuzzy_context_only.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+
+import re
+
+class Sieve (object):
+    """Unfuzzy when only a context difference to previous."""
+
+    def __init__ (self, options, global_options):
+
+        self.nmatch = 0
+
+        # Add flag indicating unreviewed context?
+        self.flag_review = True
+        if "no-review" in options:
+            options.accept("no-review")
+            self.flag_review = False
+
+        # Indicators to the caller:
+        # - monitor to avoid unnecessary reformatting when unfuzzied
+        self.caller_monitored = True
+
+
+    def process (self, msg, cat):
+
+        if msg.fuzzy \
+        and msg.msgid == msg.msgid_previous \
+        and msg.msgid_plural == msg.msgid_plural_previous:
+            msg.fuzzy = False
+            if self.flag_review:
+                msg.flag.add(u"unreviewed-context")
+            self.nmatch += 1
+            msg.modcount = 1 # in case of non-monitored messages
+
+
+    def finalize (self):
+
+        if self.nmatch > 0:
+            print "Total unfuzzied due to context: %d" % (self.nmatch,)
