cbd0ce9d5a8303dc7e1aa87b4f99b41efe1e8fb3
Chusslove Illich <caslav.ilic@gmx.net>
2008-01-01 12:24:00 +0000

    Comment on rationale.
    svn path=/trunk/l10n-kde4/scripts/pology/; revision=755382

--- sieve/unfuzzy_context_only.py
+++ sieve/unfuzzy_context_only.py
@@ -25,6 +25,8 @@ class Sieve (object):
         and msg.msgid_plural == msg.msgid_plural_previous:
             msg.fuzzy = False
             if self.flag_review:
+                # Add as manual comment, as any other type will vanish
+                # when catalog is merged with template.
                 msg.manual_comment.append(u"unreviewed-context")
             self.nmatch += 1
             msg.modcount = 1 # in case of non-monitored messages
