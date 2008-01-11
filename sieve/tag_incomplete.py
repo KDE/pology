01a2897a2517b6c56e305734b016ea988dc2791e
Chusslove Illich <caslav.ilic@gmx.net>
2008-01-11 15:04:04 +0000

    Sieve to tag incomplete messages: add incomplete-flag on either untranslated or fuzzy messages.
    svn path=/trunk/l10n-kde4/scripts/pology/; revision=759944

---
+++ sieve/tag_incomplete.py
@@ -0,0 +1,68 @@
+# -*- coding: UTF-8 -*-
+
+
+# Summit: Dig out the set of branches for the message.
+def _get_summit_branches (msg):
+
+    # Prefix of auto-comment with list of branches for the message.
+    br_prefix = "+>"
+
+    for c in msg.auto_comment:
+        if c.startswith(br_prefix):
+            return set(c[len(br_prefix):].strip().split())
+
+    return set()
+
+
+class Sieve (object):
+    """Add flag "incomplete" to untranslated and fuzzy messages."""
+
+    def __init__ (self, options, global_options):
+
+        self.nmatch = 0
+
+        # Summit: consider only messages belonging to given branches.
+        self.branches = None
+        if "branch" in options:
+            self.branches = set(options["branch"].split(","))
+            options.accept("branch")
+
+        # Indicators to the caller:
+        # - monitor to avoid unnecessary reformatting when unfuzzied
+        self.caller_monitored = True
+
+
+    def process (self, msg, cat):
+
+        flag_incomplete = u"incomplete"
+
+        # Skip obsolete messages.
+        if msg.obsolete:
+            return
+
+        # Summit: if branches were given, considered the message for
+        # tagging based on whether it belongs to any of the given branches.
+        may_tag = True
+        if self.branches:
+            msg_branches = _get_summit_branches(msg)
+            if not set.intersection(self.branches, msg_branches):
+               may_tag = False
+
+        # Add flag if message may be tagged and is not translated.
+        if may_tag and not msg.translated:
+            msg.flag.add(flag_incomplete)
+            self.nmatch += 1
+            msg.modcount = 1 # in case of non-monitored messages
+
+        # Remove flag if already there, but the message is either
+        # not to be tagged or no longer incomplete.
+        elif flag_incomplete in msg.flag and (not may_tag or msg.translated):
+            msg.flag.remove(flag_incomplete)
+            msg.modcount = 1 # in case of non-monitored messages
+
+
+    def finalize (self):
+
+        if self.nmatch > 0:
+            print "Total incomplete tagged: %d" % (self.nmatch,)
+
