705b0aacfbd7829d435746898950699967006601
Sébastien Renard <renard@kde.org>
2008-01-26 16:10:03 +0000

    Fix missing parameter in Exception init
    svn path=/trunk/l10n-kde4/scripts/pology/; revision=766776

--- misc/timeout.py
+++ misc/timeout.py
@@ -8,7 +8,7 @@ import signal
 
 class TimedOutException(Exception):
     def __init__(self, value = "Timed Out"):
-        Exception.__init__()
+        Exception.__init__(self)
         self.value = value
     def __str__(self):
         return repr(self.value)
