af0aa83a5e096715b20a19f0d7b880dc6df1c71e
Sébastien Renard <renard@kde.org>
2008-03-28 19:59:51 +0000

    New sieve to check PO message grammar with LanguageTool (http://www.languagetool.org).
    svn path=/trunk/l10n-kde4/scripts/pology/; revision=791231

---
+++ sieve/check_grammar.py
@@ -0,0 +1,71 @@
+# -*- coding: UTF-8 -*-
+
+"""
+Sieves messages with the LanguageTool grammar checker (http://www.languagetool.org)
+
+@author: Sébastien Renard <sebastien.renard@digitalfox.org>
+@license: GPLv3
+"""
+
+from httplib import HTTPConnection
+from urllib import urlencode
+from xml.dom.minidom import parseString
+from pology.misc.colors import BOLD, RED, RESET
+
+REQUEST="/?language=%s&%s"
+
+class Sieve (object):
+    """Process messages through the LanguageTool grammar checker"""
+    
+    def __init__ (self, options, global_options):
+    
+        self.nmatch = 0 # Number of match for finalize
+        self.connection=None # Connection to LanguageTool server
+
+        if "lang" in options:
+            options.accept("lang")
+            self.lang=options["lang"]
+        else:
+            self.lang="fr"
+        
+        # LanguageTool server hostname
+        if "host" in options:
+            options.accept("host")
+            host=options["host"]
+        else:
+            host="localhost"
+        
+        # LanguageTool server tcp port
+        if "port" in options:
+            options.accept("port")
+            host=options["port"]
+        else:
+            #TODO: autodetect tcp port by reading LanguageTool config file if host is localhost
+            port="8081"
+        
+        # Create connection to the LanguageTool server
+        self.connection=HTTPConnection(host, port)
+    
+    def process (self, msg, cat):
+
+        if msg.obsolete:
+            return
+
+        for msgstr in msg.msgstr:
+            self.connection.request("GET", REQUEST % (self.lang, urlencode({"text":msgstr})))
+            response=self.connection.getresponse()
+            if response:
+                responseData=response.read()
+                if "error" in responseData:
+                    self.nmatch+=1
+                    dom=parseString(responseData)
+                    for error in dom.getElementsByTagName("error"):
+                        print BOLD+"Rule: "+RESET+error.getAttribute("ruleId")
+                        print BOLD+"Message: "+RESET+error.getAttribute("msg")
+                        print BOLD+"Context: "+RESET+error.getAttribute("context")
+                        print 
+
+    def finalize (self):
+        if self.nmatch:
+            print "----------------------------------------------------"
+            print "Total matching: %d" % self.nmatch
