bd83b57fa0b38f950b9ef4b23e68f6f550ac2089
Chusslove Illich <caslav.ilic@gmx.net>
2008-04-13 21:37:24 +0000

    Update doc generation script.
    svn path=/trunk/l10n-kde4/scripts/pology/; revision=796613

---
+++ doc/makeApiDoc.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Generate API documentation from Python sources with the Epydoc tool
+
+cd $(dirname $0)
+
+if test x`which epydoc` = x; then
+    echo "Epydoc not found. Please install it from Epydoc website : http://epydoc.sf.net"
+    echo "or use your distribution package (apt-get install python-epydoc or yum install epydoc)"
+    exit 1
+fi
+
+# Proper module path for epydoc to follow.
+export PYTHONPATH=../../:$PYTHONPATH
+
+rm -rf doc/*
+
+epydoc pology \
+       -o html -v \
+       --no-sourcecode --no-frames --no-private --exclude=external
+
+# Kill generation timestamps, to not have diffs just due to it.
+  find html -iname \*.html \
+| xargs perl -pi -e 's/(Generated\b.*?) *on\b.*?(<|$)/$1$2/'
+
