4440400377bd627b1b81dcaa2206d151130db4de
Chusslove Illich <caslav.ilic@gmx.net>
2008-04-13 21:06:03 +0000

    Add module scaffolding.
    svn path=/trunk/l10n-kde4/scripts/pology/; revision=796603

---
+++ scripts/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+# pology.__init__
+
+"""
+Standalone scripts for processing PO files and Pology-related service.
+
+@author: Sébastien Renard <sebastien.renard@digitalfox.org>
+@license: GPLv3
+"""
