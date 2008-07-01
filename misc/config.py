bf04e80cfe50aa4cc9be80ff3f7155d44c3d6aa5
Chusslove Illich <caslav.ilic@gmx.net>
2008-07-01 18:14:48 +0000

    User configuration for Pology.
    svn path=/trunk/l10n-kde4/scripts/pology/; revision=826876

---
+++ misc/config.py
@@ -0,0 +1,188 @@
+# -*- coding: UTF-8 -*-
+
+"""
+Access user configuration for Pology.
+
+User can modify default aspects of Pology's behavior through a configuration
+file named C{.pologyrc} and residing in the user's home directory.
+This file is divided into sections and fileds in the INI-style format,
+for example::
+
+    # let's have some apples for the moment
+    [fruit]
+    sort = apples
+    amount = 10
+
+The configuration file is expected to be UTF-8 encoded.
+
+This module as whole is treated as the single object for accessing the
+configuration. The API reflects division into sections, with each section
+containing configuration fields::
+
+    >>> import pology.misc.config as config
+    >>> fruit = config.section("fruit")
+    >>> fruit.string("sort")
+    'apples'
+    >>> fruit.integer("amount")
+    10
+    >>>
+
+At every place where the configuration is sourced, the API documentation
+should state which sections and fields (with their types) are accessed,
+how they are used, and what is the behavior when they are not set.
+
+@author: Chusslove Illich (Часлав Илић) <caslav.ilic@gmx.net>
+@license: GPLv3
+"""
+
+import os
+import codecs
+from ConfigParser import SafeConfigParser
+
+from pology.misc.report import error
+
+
+_config = SafeConfigParser()
+
+def _parse_config ():
+
+    # Try to correctly resolve the config file location across systems.
+    cfgbase = "pologyrc"
+    if os.name=="nt":
+        cfgpath = os.path.join(os.environ.get("APPDATA", ""), cfgbase)
+    else:
+        cfgbase = "." + cfgbase
+        cfgpath = os.path.join(os.environ.get("HOME", ""), cfgbase)
+
+    # Parse the config if available.
+    if os.path.isfile(cfgpath):
+        ifl = codecs.open(cfgpath, "r", "UTF-8")
+        _config.readfp(ifl)
+        ifl.close()
+
+# Parse configuration on first import.
+_parse_config()
+
+
+class section:
+    """
+    Section of the configuration.
+
+    All getter methods take the field name and the default value,
+    which is returned when the field is not set.
+    If the configuration field is set but cannot be converted into
+    a value of requested type, execution aborts with an error message.
+
+    @ivar name: name of the section
+    @type name: string
+    """
+
+    def __init__ (self, name):
+        """
+        Retrieve a section of the configuration.
+
+        Constructed section object is valid even when the configuration does
+        not contain the requested section (in that case, all field queries
+        on the section are going to return default values).
+
+        @param name: name of the section
+        @type name: string
+        """
+
+        self.name = name
+
+
+    def _value (self, typ, name, default=None, typename=None):
+
+        if not _config.has_option(self.name, name):
+            return default
+
+        value = _config.get(self.name, name)
+        if typ is not bool:
+            try:
+                cvalue = typ(value)
+            except:
+                cvalue = None
+        else:
+            cvalue = strbool(value)
+
+        if cvalue is None:
+            if typename:
+                error(  "user-config: value '%s' of field '%s' in section '%s' "
+                        "cannot be converted into '%s' type"
+                      % (value, name, self.name, typename))
+            else:
+                error(  "user-config: value '%s' of field '%s' in section '%s' "
+                        "cannot be converted into requested type"
+                      % (value, name, self.name))
+
+        return cvalue
+
+
+    def string (self, name, default=None):
+        """
+        Get a configuration field as a string.
+
+        @rtype: unicode or as C{default}
+        """
+
+        return self._value(unicode, name, default, "string")
+
+
+    def integer (self, name, default=None):
+        """
+        Get a configuration field as an integer number.
+
+        @rtype: int or as C{default}
+        """
+
+        return self._value(int, name, default, "integer")
+
+
+    def real (self, name, default=None):
+        """
+        Get a configuration field as a real number.
+
+        @rtype: float or as C{default}
+        """
+
+        return self._value(float, name, default, "real")
+
+
+    def boolean (self, name, default=None):
+        """
+        Get a configuration field as a boolean.
+
+        @rtype: bool or as C{default}
+        """
+
+        return self._value(bool, name, default, "boolean")
+
+
+def strbool (value):
+    """
+    Parse the string specification of a boolean value.
+
+    Values considered C{false} are: C{"0"}, C{"no"}, C{"false"}, C{"off"};
+    and C{True}: C{1}, C{"yes"}, C{"true"}, C{"on"}.
+    String is stripped of leading and trailing whitespace and lowercased
+    before matching.
+
+    If the string matches none of the expected logical specifiers,
+    C{None} is returned.
+
+    @param value: string to parse
+    @type value: string
+
+    @return: parsed boolean
+    @rtype: bool
+    """
+
+    value = value.strip().lower()
+    if value in ("0", "no", "false", "off"):
+        return False
+    elif value in ("1", "yes", "true", "on"):
+        return True
+    else:
+        return None
+
