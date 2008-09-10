f980f1b98a4489826badb5a55a79273f653636a1
Chusslove Illich <caslav.ilic@gmx.net>
2008-09-10 02:31:17 +0000

    Refactored wrapping functions. Function to adapt spans from match done on filtered text onto original text.
    svn path=/trunk/l10n-support/pology/; revision=859328

--- misc/diff.py
+++ misc/diff.py
@@ -12,18 +12,39 @@ from pology.misc.split import split_text
 import re
 from difflib import ndiff
 
-_opn_ch = u"{"
-_cls_ch = u"}"
-_pls_ch = u"+"
-_mns_ch = u"-" #–
 
-def diff_texts (text_old, text_new, markup=False, format=None):
+_new_tag = "+"
+
+_new_vtag = "+"
+_new_opnc = "{"
+_new_clsc = "}"
+
+_old_tag = "-"
+
+_old_vtag = "-"
+_old_opnc = "{"
+_old_clsc = "}"
+
+_equ_tag = " "
+
+
+def word_diff (text_old, text_new, markup=False, format=None):
     """
-    Create text with embedded differences between old and new texts.
+    Create word-level difference between old and new text.
 
     The difference is computed by looking at texts as collections of words
-    and intersegments. Segments present only in the new text are embedded
-    as C{{+...+}}, and segments only in the old text as C{{-...-}}.
+    and intersegments. Difference is presented as a list of tuples,
+    with each tuple composed of a difference tag and a text segment.
+    Difference tag is string C{"+"}, C{"-"}, or C{" "}, for text segments
+    which are new, old, or present in both texts, respectively.
+    The list is ordered such that joining all text segments not marked
+    as old will reconstruct the new text, and joining all not marked
+    as new will reconstruct the old text. For example::
+
+        >>> s1 = "A new type of foo."
+        >>> s2 = "A new kind of bar."
+        >>> text_diff(s1, s2)
+        []
 
     Differencing may take into account when the texts are expected to have
     XML-like markup, or when they are of certain format defined by Gettext.
@@ -44,8 +65,8 @@ def diff_texts (text_old, text_new, markup=False, format=None):
     @param format: Gettext format flag (e.g. C{"c-format"}, etc.)
     @type format: string
 
-    @returns: string with embedded differences and difference ratio
-    @rtype: string, float
+    @returns: difference list and difference ratio
+    @rtype: list of tuples, float
     """
 
     # Split text into segments: words and intersections, combined into
@@ -113,52 +134,55 @@ def diff_texts (text_old, text_new, markup=False, format=None):
         if seq_old:
             ndlist.append("- " + "".join([x[2:] for x in seq_old]))
     dlist = ndlist
-    dlist_intr = None # no longer valid
 
-    # Format the embedded output.
+    # Join contiguous new/old/both segments, make tagged tuples.
+    ndlist = []
     S_EQU, S_NEW, S_OLD = range(3)
-    dtext = type(text_new)("")
     state = S_EQU
+    cseg = []
     len_equ, len_old, len_new = 0, 0, 0
+    dlist.append(". ") # sentry
     for el in dlist:
-        if state == S_EQU:
-            if el.startswith("+"):
-                dtext += _opn_ch + _pls_ch
+        segtype = el[0]
+
+        if state == S_EQU and segtype in ("+", "-", "."):
+            if cseg:
+                ndlist.append((_equ_tag, "".join(cseg)))
+            cseg = []
+            if segtype == "+":
                 state = S_NEW
-            elif el.startswith("-"):
-                dtext += _opn_ch + _mns_ch
+            else:
                 state = S_OLD
 
-        elif state == S_OLD:
-            if el.startswith(" "):
-                dtext += _mns_ch + _cls_ch
+        elif state == S_OLD and segtype in (" ", "+", "."):
+            if cseg:
+                ndlist.append((_old_tag, "".join(cseg)))
+            cseg = []
+            if segtype == " ":
                 state = S_EQU
-            elif el.startswith("+"):
-                dtext += _mns_ch + _cls_ch + _opn_ch + _pls_ch
+            else:
                 state = S_NEW
 
-        elif state == S_NEW:
-            if el.startswith(" "):
-                dtext +=  _pls_ch + _cls_ch
+        elif state == S_NEW and segtype in (" ", "-", "."):
+            if cseg:
+                ndlist.append((_new_tag, "".join(cseg)))
+            cseg = []
+            if segtype == " ":
                 state = S_EQU
-            elif el.startswith("-"):
-                dtext += _pls_ch + _cls_ch + _opn_ch + _mns_ch
+            else:
                 state = S_OLD
 
         seg = el[2:]
-        if el.startswith("-"):
+        if segtype == "-":
             len_old += len(seg)
-        elif el.startswith("+"):
+        elif segtype == "+":
             len_new += len(seg)
         else:
             len_equ += len(seg)
+        if seg:
+            cseg.append(seg)
 
-        dtext += seg
-
-    if state == S_OLD:
-        dtext += _mns_ch + _cls_ch
-    elif state == S_NEW:
-        dtext += _pls_ch + _cls_ch
+    dlist = ndlist
 
     len_all = len_new + len_old + len_equ
     if len_all > 0:
@@ -166,16 +190,48 @@ def diff_texts (text_old, text_new, markup=False, format=None):
     else:
         diff_ratio = 0.0
 
+    return dlist, diff_ratio
+
+
+def word_ediff (text_old, text_new, markup=False, format=None):
+    """
+    Create word-level embedded difference between old and new texts.
+
+    Same as L{word_diff}, but the difference is returned as text in
+    which the new segments are wrapped as C{{+...+}}, and the old
+    segments as C{{-...-}}.
+
+    @returns: string with embedded differences and difference ratio
+    @rtype: string, float
+
+    @see: L{word_diff}
+    """
+
+    dlist, diff_ratio = word_diff(text_old, text_new, markup, format)
+    dtext = []
+    for segtag, segtext in dlist:
+        if segtag == _new_tag:
+            dtext.append(  _new_opnc + _new_vtag
+                         + segtext
+                         + _new_vtag + _new_clsc)
+        elif segtag == _old_tag:
+            dtext.append(  _old_opnc + _old_vtag
+                         + segtext
+                         + _old_vtag + _old_clsc)
+        else:
+            dtext.append(segtext)
+    dtext = "".join(dtext)
+
     return dtext, diff_ratio
 
 
-_capt_old_rx = re.compile(  "\\" + _opn_ch + "\\" + _mns_ch + "(.*?)" \
-                          + "\\" + _mns_ch + "\\" + _cls_ch, re.U|re.S)
-_capt_new_rx = re.compile(  "\\" + _opn_ch + "\\" + _pls_ch + "(.*?)" \
-                          + "\\" + _pls_ch + "\\" + _cls_ch, re.U|re.S)
+_capt_old_rx = re.compile(  "\\" + _old_opnc + "\\" + _old_vtag + "(.*?)" \
+                          + "\\" + _old_vtag + "\\" + _old_clsc, re.U|re.S)
+_capt_new_rx = re.compile(  "\\" + _new_opnc + "\\" + _new_vtag + "(.*?)" \
+                          + "\\" + _new_vtag + "\\" + _new_clsc, re.U|re.S)
 
 
-def diff_to_old (dtext):
+def ediff_to_old (dtext):
     """
     Recover old version (-) from text with embedded differences.
 
@@ -185,7 +241,7 @@ def diff_to_old (dtext):
     @returns: old version of the text
     @rtype: string
 
-    @see: L{diff_texts}
+    @see: L{word_ediff}
     """
 
     text = dtext
@@ -194,7 +250,7 @@ def diff_to_old (dtext):
     return text
 
 
-def diff_to_new (dtext):
+def ediff_to_new (dtext):
     """
     Recover new version (+) from text with embedded differences.
 
@@ -204,11 +260,110 @@ def diff_to_new (dtext):
     @returns: new version of the text
     @rtype: string
 
-    @see: L{diff_texts}
+    @see: L{word_ediff}
     """
 
     text = dtext
     text = _capt_new_rx.sub("\\1", text)
     text = _capt_old_rx.sub("", text)
     return text
+
+
+def adapt_spans (otext, ftext, spans, merge=True):
+    """
+    Adapt matched spans in filtered text to original text.
+
+    Sometimes text gets filtered before being matched, and when a match
+    is found in the filtered text, it needs to be reported relative to
+    the original text. This function will heuristically adapt matched spans
+    relative to the filtered text back to the original text.
+
+    Spans are given as list of index tuples C{[(start1, end1), ...]} where
+    start and end index have standard Python semantics (may be negative too).
+    If C{merge} is C{True}, any spans that overlap or abut after adaptation
+    will be merged into a single span, ordered by increasing start index,
+    and empty spans removed; otherwise each adapted span will strictly
+    correspond to the input span at that position.
+
+    If any of the input spans are invalid, the results are undefined.
+
+    @param otext: original text
+    @type otext: string
+    @param ftext: filtered text
+    @type ftext: string
+    @param spans: matched spans
+    @type spans: list of index tuples
+    @param merge: whether to merge overlapping spans
+    @type bool
+
+    @returns: adapted spans
+    @rtype: list of index tuples
+    """
+
+    # Resolve negative spans.
+    flen = len(ftext)
+    fspans = []
+    for start, end in spans:
+        if start < 0:
+            start = flen - start
+        if end < 0:
+            end = flen - end
+        fspans.append((start, end))
+
+    # Create character-level difference from original to filtered text.
+    dlist = list(ndiff(otext, ftext))
+    dlist = [x for x in dlist if x[:1] in "+- "] # remove non-diff segments
+    dlist = [(x[:1], x[2:]) for x in dlist] # split into (type, segment) pairs
+
+    # For each span, go through the difference and... do some magic.
+    aspans = []
+    for fspan in fspans:
+        #print "======> span to adapt:", fspan
+        aspan = []
+        for filtered_index, end_extra in zip(fspan, (0, 1)):
+            #print ">>> index to adapt:", filtered_index
+            original_index = 0
+            track_index = 0
+            for dtype, dseg in dlist:
+                slen = len(dseg)
+                if dtype == "+":
+                    track_index += slen
+                elif dtype == "-":
+                    original_index += slen
+                else:
+                    original_index += slen
+                    track_index += slen
+                #print dtype, dseg, track_index, original_index
+                if dtype not in ("+", "-"):
+                    if track_index + end_extra > filtered_index:
+                        original_index -= track_index - filtered_index
+                        break
+            #print "<<< adapted index:", original_index
+            aspan.append(original_index)
+
+        aspans.append(tuple(aspan))
+
+    # Merge spans if requested.
+    if merge:
+        maspans = []
+        while len(aspans) > 0:
+            cstart, cend = aspans[0]
+            if cstart >= cend:
+                aspans.pop(0) # remove empty spans
+                continue
+            i = 0
+            while i < len(aspans):
+                start, end = aspans[i]
+                if cend >= start and cstart <= end:
+                    cstart = min(cstart, start)
+                    cend = max(cend, end)
+                    aspans.pop(i)
+                else:
+                    i += 1
+            maspans.append((cstart, cend))
+        # Sort by start index.
+        maspans.sort(lambda x, y: cmp(x[0], y[0]))
+        aspans = maspans
+
+    return aspans
 
