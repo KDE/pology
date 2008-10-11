300509856e57eae66f29ad2982cae81548b2eac1
Chusslove Illich <caslav.ilic@gmx.net>
2008-10-11 12:20:06 +0000

    In hook factories that read entity definitions, provide the exact environment variable name where paths are stored, rather than the language code inserted into implied variable name.
    svn path=/trunk/l10n-support/pology/; revision=869918

--- hook/check_xml.py
+++ hook/check_xml.py
@@ -11,9 +11,10 @@ from pology.misc.comments import manc_parse_flag_list
 from pology.sieve.check_xml_kde4 import check_xml_kde4 as check_xml_kde4_base
 from pology.sieve.check_xml_kde4 import flag_no_check_xml
 from pology.misc.resolve import read_entities
+from pology.misc.report import warning
 
 
-def check_xml_kde4 (strict=False, entities={}, entlang=None, fcap=True):
+def check_xml_kde4 (strict=False, entities={}, entpathenv=None, fcap=True):
     """
     Check XML markup in translations of KDE4 UI catalogs.
 
@@ -25,10 +26,8 @@ def check_xml_kde4 (strict=False, entities={}, entlang=None, fcap=True):
     A dictionary of external XML entities (name, value) may be provided
     if such appear in the translation, for the check not to warn about
     unknown entities.
-    Entities may also be automatically collected for a given language code,
-    by parsing all C{*.entities} files in directory paths given by
-    the environment variable C{ENTS_KDE4_UI_<ENTLANG>},
-    where C{ENTLANG} is C{entlang} converted to all-caps;
+    Entities may also be automatically collected, by parsing all C{*.entities}
+    files in directory paths given by the environment variable C{entpathenv};
     entity files are taken only from the roots of directory paths,
     and not recursively searched for.
     If an entity with the first letter in uppercase is encountered and not
@@ -42,18 +41,18 @@ def check_xml_kde4 (strict=False, entities={}, entlang=None, fcap=True):
     @type strict: bool
     @param entities: additional entities to consider as known
     @type entities: dict
-    @param entlang: language code for which to fetch additional entities
-    @type entlang: string
+    @param entpathenv: environment variable with paths to entity definitions
+    @type entpathenv: string
     @param fcap: whether to allow first-uppercase entities
     @type fcap: bool
 
     @note: Hook type factory: C{(cat, msg, text) -> None}
     """
 
-    return _check_xml_kde4_w(strict, entities, entlang, fcap, False)
+    return _check_xml_kde4_w(strict, entities, entpathenv, fcap, False)
 
 
-def check_xml_kde4_sp (strict=False, entities={}, entlang=None, fcap=False):
+def check_xml_kde4_sp (strict=False, entities={}, entpathenv=None, fcap=False):
     """
     Like L{check_xml_kde4}, except that erroneous spans are returned
     instead of reporting problems to stdout.
@@ -61,20 +60,25 @@ def check_xml_kde4_sp (strict=False, entities={}, entlang=None, fcap=False):
     @note: Hook type factory: C{(cat, msg, text) -> spans}
     """
 
-    return _check_xml_kde4_w(strict, entities, entlang, fcap, True)
+    return _check_xml_kde4_w(strict, entities, entpathenv, fcap, True)
 
 
-def _check_xml_kde4_w (strict, entities, entlang, fcap, spanrep):
+def _check_xml_kde4_w (strict, entities, entpathenv, fcap, spanrep):
     """
     Worker for C{check_xml_kde4*} hook factories.
     """
 
-    if entlang is not None:
-        entities = entities.copy()
-        entpath = os.getenv("ENTS_KDE4_UI_%s" % entlang.upper(), "")
-        for path in entpath.split(":"):
-            pattern = os.path.join(path, "*.entities")
-            entities.update(read_entities(*glob.glob(pattern)))
+    if entpathenv is not None:
+        entpath = os.getenv(entpathvar)
+        if entpath is None:
+            warning("expected environment variable %s not set" % entpathenv)
+        else:
+            entities_by_arg = entities # add in the end, has higher priority
+            entities = {}
+            for path in entpath.split(":"):
+                pattern = os.path.join(path, "*.entities")
+                entities.update(read_entities(*glob.glob(pattern)))
+            entities.update(entities_by_arg)
     if fcap:
         fcap_entities = {}
         for name, value in entities.iteritems():
