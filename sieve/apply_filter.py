b210f668445780e2d3e9ad2c124995341afedaf9
Chusslove Illich <caslav.ilic@gmx.net>
2008-10-22 14:17:03 +0000

    Allow application of F3A, F3C hooks as well, next to F1A.
    svn path=/trunk/l10n-support/pology/; revision=874843

--- sieve/apply_filter.py
+++ sieve/apply_filter.py
@@ -3,26 +3,36 @@
 """
 Apply filters to translation.
 
-Modify C{msgstr} fields by filtering them through a combination of
-pure text hooks (C{(text) -> text} hook type) from global C{pology.hook}
-and language specific C{pology.l10n.<lang>.hook} modules.
+Modify C{msgstr} fields by passing them through a combination of
+filtering L{hooks<hook>} of type F1A (C{(text) -> text})
+or F3* (C{(text/msgstr, msg, cat)->msgstr}).
 
-Sieve options:
-  - C{filter:<filter>,...}: comma-separated list of pure text hooks
+Sieve parameters:
+  - C{filter:<filter>,...}: comma-separated list of hook specifications
+  - C{factory:<filter>~~...}: double-tilde separated list of factory arguments
 
-Global hooks are specified just by their module name, e.g. C{foo}
-for C{pology.hook.foo}, if the hook is the default C{process()} function,
-while C{foo/bar} for a method C{bar()} representing the hook.
+Global hooks can be specified just by their module name, e.g. C{foo}
+for C{pology.hook.foo}, if the module defines hook as C{process()} function;
+while using C{foo/bar} for if the hook is given by the C{bar()} function.
 Language specific hooks are preceded by the language code, e.g. C{ll:foo} for
 C{process()} from the C{pology.l10n.ll.hook.foo} module,
 and C{ll:foo/bar} for C{bar()} within the same module.
-Hooks are applied in the order given by the C{filter} option.
+Hooks are applied in the order given by the C{filter} parameter.
+
+If the specified hook is in fact a hook factory, C{factory} parameter
+is used to specify factory arguments. The number of factory argument strings
+(separated by C{~~}) must be equal to number of filters.
+Each argument string is an ansamble of arguments as would be given
+to the factory call inside Python code (without wrapping parenthesis).
+If an argument string is empty, the hook corresponding to it is considered
+a plain hook rather than factory, and skipped on evaluation of factories.
 
 @author: Chusslove Illich (Часлав Илић) <caslav.ilic@gmx.net>
 @license: GPLv3
 """
 
 from pology.misc.langdep import get_hook_lreq
+from pology.misc.report import report, warning, error
 
 
 class Sieve (object):
@@ -33,7 +43,19 @@ class Sieve (object):
         if "filter" in options:
             options.accept("filter")
             freqs = options["filter"].split(",")
-            self.tfilters = [get_hook_lreq(x, abort=True) for x in freqs]
+            self.tfilters = [[get_hook_lreq(x, abort=True), x] for x in freqs]
+
+        # After filtering hooks have been loaded.
+        if "factory" in options:
+            options.accept("factory")
+            fargs = options["factory"].split("~~")
+            if len(fargs) != len(self.tfilters):
+                error("number of filters (%d) and factories (%d) "
+                      "does not match" % (len(self.tfilters), len(fargs)))
+            for i in range(len(self.tfilters)):
+                if fargs[i]: # empty strings not factories
+                    factory = self.tfilters[i][0]
+                    self.tfilters[i][0] = eval("factory(%s)" % fargs[i])
 
         # Number of modified messages.
         self.nmod = 0
@@ -44,8 +66,15 @@ class Sieve (object):
         mcount = msg.modcount
 
         for i in range(len(msg.msgstr)):
-            for tfilter in self.tfilters:
-                msg.msgstr[i] = tfilter(msg.msgstr[i])
+            for tfilter, tfname in self.tfilters:
+                try: # try as type F1A hook
+                    msg.msgstr[i] = tfilter(msg.msgstr[i])
+                except TypeError:
+                    try: # try as type F3* hook
+                        msg.msgstr[i] = tfilter(msg.msgstr[i], msg, cat)
+                    except TypeError:
+                        warning("cannot execute filter '%s'" % tfname)
+                        raise
 
         if mcount < msg.modcount:
             self.nmod += 1
@@ -54,5 +83,5 @@ class Sieve (object):
     def finalize (self):
 
         if self.nmod:
-            print "Total modified by filtering: %d" % self.nmod
+            report("Total modified by filtering: %d" % self.nmod)
 
