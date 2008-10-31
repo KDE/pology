15e4b83531eaa7bdd72b48ecbfd0611344ed5ab7
Chusslove Illich <caslav.ilic@gmx.net>
2008-10-31 14:58:39 +0000

    Switch parameter handling to new style.
    svn path=/trunk/l10n-support/pology/; revision=878201

--- sieve/find_messages.py
+++ sieve/find_messages.py
@@ -1,14 +1,14 @@
 # -*- coding: UTF-8 -*-
 
 """
-Find messages by regular expression matching.
+Find messages in catalogs.
 
-Matches the regular expression against requested elements of the message,
-and reports the message if the match exists. Every matched message is
-reported to standard output, with the name of the file from which it comes,
+Matches patterns against elements of the message and examines its properties,
+and reports the message if the match is complete. Matched messages are
+reported to standard output, with the name of the file from which they come,
 and referent line and entry number within the file.
 
-Sieve options for matching:
+Sieve parameters for matching:
   - C{msgctxt:<regex>}: regular expression to match against the C{msgctxt}
   - C{msgid:<regex>}: regular expression to match against the C{msgid}
   - C{msgstr:<regex>}: regular expression to match against the C{msgstr}
@@ -16,7 +16,7 @@ Sieve options for matching:
   - C{transl}: the message must be translated
   - C{plural}: the message must be plural
 
-If more than one of the matching options are given (e.g. both C{msgid} and
+If more than one of the matching parameters are given (e.g. both C{msgid} and
 C{msgstr}), the message matches only if all of them match. In case of plural
 messages, C{msgid} is considered matched if either C{msgid} or C{msgid_plural}
 fields match, and C{msgstr} if any of the C{msgstr} fields match.
@@ -26,12 +26,12 @@ by which the meaning of the match is inverted; for example, if both
 C{msgid:foo} and C{nmsgid:bar} are given, then the message matches
 if its C{msgid} contains C{foo} but does not contain C{bar}.
 
-Sieve options for replacement:
+Sieve parameters for replacement:
   - C{replace:<string>}: string to replace matched part of translation
 
 The C{replace} option must go together with the C{msgstr} match. As usual for regular expression replacement, the replacement string may contain C{\<number>} references to groups defined by C{msgstr} match.
 
-Other sieve options:
+Other sieve parameters:
   - C{accel:<chars>}: strip these characters as accelerator markers
   - C{case}: case-sensitive match (insensitive by default)
   - C{mark}: mark each matched message with a flag
@@ -63,7 +63,10 @@ when it is not the default C{process()} within the hook module.
 @license: GPLv3
 """
 
-import sys, os, re
+import sys
+import os
+import re
+
 from pology.misc.report import error, warning
 from pology.misc.msgreport import report_msg_content
 from pology.misc.langdep import get_hook_lreq
@@ -72,94 +75,175 @@ from pology.file.message import MessageUnsafe
 from pology.hook.remove_subs import remove_accel_msg
 
 
+def setup_sieve (p):
+
+    p.set_desc(
+    "Find and display messages."
+    "\n\n"
+    "Each message is matched according to one or several criteria, "
+    "and if it matches as whole, its content is displayed to output device, "
+    "along with originating catalog and referent line and entry number."
+    "\n\n"
+    "When several matching parameters are given, by default a message "
+    "is matched if all of them match (AND-semantics). "
+    "This can be changed to OR-semantics in a limited sense, "
+    "for pattern matching in text fields (msgid, msgstr, etc.) "
+    "using the '%(par1)s' parameter. "
+    "Any matching parameter can be repeated if it makes sense (e.g. two "
+    "matches on msgid)."
+    "\n\n"
+    "See documentation to pology.sieve.find_messages for details."
+    % dict(par1="or")
+    )
+
+    p.add_param("msgid", unicode, multival=True,
+                metavar="REGEX",
+                desc=
+    "Matches if the msgid field matches the regular expression."
+    )
+    p.add_param("nmsgid", unicode, multival=True,
+                metavar="REGEX",
+                desc=
+    "Matches if the msgid field does not match the regular expression."
+    )
+    p.add_param("msgstr", unicode, multival=True,
+                metavar="REGEX",
+                desc=
+    "Matches if the msgstr field matches the regular expression."
+    )
+    p.add_param("nmsgstr", unicode, multival=True,
+                metavar="REGEX",
+                desc=
+    "Matches if the msgstr field does not match the regular expression."
+    )
+    p.add_param("msgctxt", unicode, multival=True,
+                metavar="REGEX",
+                desc=
+    "Matches if the msgctxt field matches the regular expression."
+    )
+    p.add_param("nmsgctxt", unicode, multival=True,
+                metavar="REGEX",
+                desc=
+    "Matches if the msgctxt field does not match the regular expression."
+    )
+    p.add_param("comment", unicode, multival=True,
+                metavar="REGEX",
+                desc=
+    "Matches if a comment line (extracted or translator) "
+    "matches the regular expression."
+    )
+    p.add_param("ncomment", unicode, multival=True,
+                metavar="REGEX",
+                desc=
+    "Matches if a comment line (extracted or translator) "
+    "does not match the regular expression."
+    )
+    p.add_param("transl", bool,
+                desc=
+    "Matches if the message is translated."
+    )
+    p.add_param("ntransl", bool,
+                desc=
+    "Matches if the message is not translated."
+    )
+    p.add_param("plural", bool,
+                desc=
+    "Matches if the message is plural."
+    )
+    p.add_param("nplural", bool,
+                desc=
+    "Matches if the message is not plural."
+    )
+    p.add_param("maxchar", int, defval=0,
+                metavar="NUM",
+                desc=
+    "Matches if the msgstr field has at most this many characters "
+    "(0 or less means any number of characters)."
+    )
+    p.add_param("or", bool, defval=False, attrname="or_match",
+                desc=
+    "Use OR-semantics for matching text fields: if any of "
+    "the patterns matches, the message is matched as whole."
+    )
+    p.add_param("case", bool, defval=False,
+                desc=
+    "Use case-sensitive text matching."
+    )
+    p.add_param("accel", unicode, multival=True,
+                metavar="CHAR",
+                desc=
+    "Character which is used as UI accelerator marker in text fields, "
+    "to ignore it on matching. "
+    "If a catalog defines accelerator marker in the header, "
+    "this value overrides it."
+    )
+    p.add_param("mark", bool, defval=False,
+                desc=
+    "Add '%(flag)s' flag to each matched message."
+    % dict(flag=_flag_mark)
+    )
+    p.add_param("filter", unicode, multival=True, seplist=True,
+                metavar="HOOKSPEC,...",
+                desc=
+    "F1A hook specification, to filter the msgstr fields through "
+    "before matching them. "
+    "Several hooks can be specified either as a comma-separated list, "
+    "or by repeating the parameter."
+    )
+    p.add_param("replace", unicode,
+                metavar="REPLSTR",
+                desc=
+    "Replace all substrings matched by msgstr pattern with REPLSTR. "
+    "It can include back-references to matched groups (\\1, \\2, etc.)"
+    )
+
+
 _flag_mark = u"pattern-match"
 
 
 class Sieve (object):
 
-    def __init__ (self, options, global_options):
+
+    def __init__ (self, params, options):
 
         self.nmatch = 0
 
-        self.accels = None
-        if "accel" in options:
-            options.accept("accel")
-            self.accels = list(options["accel"])
+        self.p = params
 
         self.rxflags = re.U
-        if "case" in options:
-            options.accept("case")
-        else:
+        if not self.p.case:
             self.rxflags |= re.I
 
         self.field_matches = []
         has_msgstr_match = False
         for field in ("msgctxt", "msgid", "msgstr", "comment"):
             rxstr = None
-            if field in options:
-                options.accept(field)
-                regex = re.compile(options[field], self.rxflags)
+            for value in getattr(params, field) or []:
+                regex = re.compile(value, self.rxflags)
                 self.field_matches.append((field, regex, False))
                 if field == "msgstr":
                     has_msgstr_match = True
             nfield = "n%s" % field
-            if nfield in options:
-                options.accept(nfield)
-                rxstr = options[nfield]
-                regex = re.compile(options[nfield], self.rxflags)
+            for value in getattr(params, nfield) or []:
+                regex = re.compile(value, self.rxflags)
                 self.field_matches.append((field, regex, True))
 
-        if not self.field_matches:
-            error("no search pattern for a message field given")
-
-        self.replace = None
-        if "replace" in options:
+        if self.p.replace is not None:
             if not has_msgstr_match:
                 error("no msgstr search pattern provided for replacement")
-            options.accept("replace")
-            self.replace = options["replace"]
 
-        self.mark = False
-        if "mark" in options:
-            options.accept("mark")
-            self.mark = True
-
+        # Resolve filtering hooks.
         self.pfilters = []
-        if "filter" in options:
-            options.accept("filter")
-            freqs = options["filter"].split(",")
-            self.pfilters = [get_hook_lreq(x, abort=True) for x in freqs]
-
-        self.transl = False
-        if "transl" in options:
-            options.accept("transl")
-            self.transl = True
-        self.untran = False
-        if "ntransl" in options:
-            options.accept("ntransl")
-            self.untran = True
-
-        self.plural = False
-        if "plural" in options:
-            options.accept("plural")
-            self.plural = True
-        self.nonplural = False
-        if "nplural" in options:
-            options.accept("nplural")
-            self.nonplural = True
-
-        self.maxchar = None
-        if "maxchar" in options:
-            options.accept("maxchar")
-            self.maxchar = int(options["maxchar"])
+        for hreq in self.p.filter or []:
+            self.pfilters.append(get_hook_lreq(x, abort=True))
 
         # Unless replacement or marking requested, no need to monitor/sync.
-        if self.replace is None and not self.mark:
+        if self.p.replace is None and not self.p.mark:
             self.caller_sync = False
             self.caller_monitored = False
 
         # Select wrapping for reporting messages.
-        if global_options.do_wrap:
+        if options.do_wrap:
             self.wrapf = wrap_field
         else:
             self.wrapf = wrap_field_unwrap
@@ -168,21 +252,21 @@ class Sieve (object):
     def process_header (self, hdr, cat):
 
         # Force explicitly given accelerators.
-        if self.accels is not None:
-            cat.set_accelerator(self.accels)
+        if self.p.accel is not None:
+            cat.set_accelerator(self.p.accel)
 
 
     def process (self, msg, cat):
 
         if msg.obsolete:
             return
-        if self.transl and not msg.translated:
+        if self.p.transl and not msg.translated:
             return
-        if self.untran and msg.translated:
+        if self.p.ntransl and msg.translated:
             return
-        if self.plural and not msg.msgid_plural:
+        if self.p.plural and not msg.msgid_plural:
             return
-        if self.nonplural and msg.msgid_plural:
+        if self.p.nplural and msg.msgid_plural:
             return
 
         # Prepare filtered message for matching.
@@ -197,7 +281,7 @@ class Sieve (object):
                 msgf.msgstr[i] = pfilter(msgf.msgstr[i])
 
         # Match requested fields.
-        match = True
+        match = not self.p.or_match
         hl_spec = {}
         for field, regex, invert in self.field_matches:
 
@@ -223,9 +307,9 @@ class Sieve (object):
             else:
                 error("unknown search field '%s'" % field)
 
-            if self.maxchar is not None and field in ("msgid", "msgstr"):
+            if self.p.maxchar > 0 and field in ("msgid", "msgstr"):
                 nchar = sum([len(x[0]) for x in texts]) // len(texts)
-                if nchar > self.maxchar:
+                if nchar > self.p.maxchar:
                     match = False
                     break
 
@@ -249,20 +333,23 @@ class Sieve (object):
             if invert:
                 local_match = not local_match
 
-            # Check for global match (global match is AND).
-            if not local_match:
-                match = False
+            # Check for global match.
+            if self.p.or_match:
+                match = match or local_match
+            else:
+                match = match and local_match
+            if not match:
                 break
 
             # Do the replacement in translation if requested.
             # NOTE: Use the real, not the filtered message.
-            if field == "msgstr" and self.replace is not None:
+            if field == "msgstr" and self.p.replace is not None:
                 for i in range(len(msg.msgstr)):
-                    msg.msgstr[i] = regex.sub(self.replace, msg.msgstr[i])
+                    msg.msgstr[i] = regex.sub(self.p.replace, msg.msgstr[i])
 
         if match:
             self.nmatch += 1
-            if not self.mark:
+            if not self.p.mark:
                 delim = "-" * 20
                 if self.nmatch == 1:
                     print delim
@@ -272,7 +359,7 @@ class Sieve (object):
             else:
                 msg.flag.add(_flag_mark)
 
-        elif self.mark and _flag_mark in msg.flag:
+        elif self.p.mark and _flag_mark in msg.flag:
             # Remove the flag if present but the message does not match.
             msg.flag.remove(_flag_mark)
 
