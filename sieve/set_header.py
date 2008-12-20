f3798a39ff53074585c73e2a85dd8b1ae4b1a8de
Chusslove Illich <caslav.ilic@gmx.net>
2008-12-20 14:07:33 +0000

    Converted sieve to new style. Several fields may now be specified for single run.
    svn path=/trunk/l10n-support/pology/; revision=899300

--- sieve/set_header.py
+++ sieve/set_header.py
@@ -16,14 +16,14 @@ Sieve options for setting a field:
   - C{before:<name>}: when adding a field, add it before the given field
   - C{reorder}: reinsert the field to match implied order
 
-The {field} parameter takes the exact name of the header field and
+The C{field} parameter takes the exact name of the header field and
 a value to set it to. But, by itself it will set the field only if it
 is already present in the header. To create the field if not present,
 C{create} option must be given. If the field is being added, parameters
 C{after} and C{before} can be used to specify where to insert it;
 by default the new field is appended at the end of the header.
 If the field is present, using the option C{reorder} it can be moved within
-the header to match the order implied by C{after} and {before}.
+the header to match the order implied by C{after} and C{before}.
 
 Field value strings may contain %-directives, which are expanded to
 catalog-dependent substrings prior to setting the value. These are:
@@ -43,46 +43,72 @@ import time
 from pology.misc.report import error, warning
 from pology.misc.resolve import expand_vars
 
+def setup_sieve (p):
+
+    p.set_desc(
+    "Set elements of the PO header."
+    "\n\n"
+    "Sometimes a certain header field needs to be updated throught many POs, "
+    "and this sieve serves that purpose."
+    % dict(par1="or")
+    )
+
+    p.add_param("field", unicode, multival=True,
+                metavar="FIELD:VALUE",
+                desc=
+    "Set a header field to the given value. "
+    "This parameter can be repeated, to set several fields in single run."
+    )
+    p.add_param("create", bool, defval=False,
+                desc=
+    "Add the field if not present "
+    "(by default the field is set only if it already exists in the header)."
+    )
+    p.add_param("after", unicode,
+                metavar="FIELD",
+                desc=
+    "When the new field is being added, add it after this field. "
+    "If such field does not exist, the new field is added as the last one."
+    )
+    p.add_param("before", unicode,
+                metavar="FIELD",
+                desc=
+    "When the new field is being added, add it before this field. "
+    "If such field does not exist, the new field is added as the last one."
+    )
+    p.add_param("reorder", bool, defval=False,
+                desc=
+    "If the field to be set is present, but not in the order implied by "
+    "'%(after)s' and '%(before)s' parameters, reinsert it accordingly."
+    % dict(after="after", before="before")
+    )
+
 
 class Sieve (object):
 
-    def __init__ (self, options, global_options):
+    def __init__ (self, params, options):
 
-        # Setting a field.
-        self.field = None
-        if "field" in options:
-            self.field, self.value = options["field"].split(":", 1)
-            options.accept("field")
+        # Parse field setting specifications.
+        self.fields_values = []
+        for field_value_str in (params.field or []):
+            field_value = field_value_str.split(":", 1)
+            if len(field_value) != 2:
+                error("invalid specification of header field and value: %s"
+                      % field_value_str)
+            self.fields_values.append(field_value)
+        # Set fields in reverse, so that 'after' and 'before' parameters
+        # are followed by the order of appearance of fields in command line.
+        self.fields_values.reverse()
 
-        # Should the field be created if not present?
-        self.create = False
-        if "create" in options:
-            self.create = True
-            options.accept("create")
-
-        # After or before which field to add the new one?
-        self.after = None
-        if "after" in options:
-            self.after = options["after"]
-            options.accept("after")
-        self.before = None
-        if "before" in options:
-            self.before = options["before"]
-            options.accept("before")
-
-        # Can the field be reordered?
-        self.reorder = False
-        if "reorder" in options:
-            self.reorder = True
-            options.accept("reorder")
+        self.p = params
 
 
     def process_header (self, hdr, cat):
 
-        if self.field and (self.create or hdr.select_fields(self.field)):
-            pvars = {"poname" : cat.name}
-            value = expand_vars(self.value, pvars)
-            hdr.set_field(self.field, value,
-                          after=self.after, before=self.before,
-                          reorder=self.reorder)
+        pvars = {"poname" : cat.name}
+        for field, value in self.fields_values:
+            if self.p.create or hdr.select_fields(field):
+                hdr.set_field(field, expand_vars(value, pvars),
+                              after=self.p.after, before=self.p.before,
+                              reorder=self.p.reorder)
 
