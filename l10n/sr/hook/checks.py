472fe867913264e3104fcd750998143e77ceff82
Chusslove Illich <caslav.ilic@gmx.net>
2009-01-18 15:18:59 +0000

    More tags in which Latin content is allowed.
    svn path=/trunk/l10n-support/pology/; revision=912968

--- l10n/sr/hook/checks.py
+++ l10n/sr/hook/checks.py
@@ -31,7 +31,8 @@ _no_check_lat_rxs = (
     re.compile(r"<\s*(%s)\b.*?\b\1\s*>" % "|".join("""
         bcode command envar filename icode shortcut placeholder style code tt
         literal screen option keycap userinput systemitem prompt function
-        foreignphrase varname programlisting token markup parameter
+        foreignphrase varname programlisting token markup parameter keysym
+        methodname
     """.split()), re.U|re.I|re.S),
     # - some tags are requested without attributes, as otherwise
     # Latin-content is allowed inside attributes only.
