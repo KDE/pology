13f75126b6fa3e2a4c3bda0282c62233ed0ab4bd
Chusslove Illich <caslav.ilic@gmx.net>
2009-03-01 15:00:57 +0000

    Parameters for setting header comments.
    svn path=/trunk/l10n-support/pology/; revision=933748

--- sieve/set_header.py
+++ sieve/set_header.py
@@ -3,11 +3,8 @@
 """
 Set elements of the PO header.
 
-Sometimes a certain header field needs to be updated throught all POs,
+Sometimes a certain header field or comment needs to be updated throughout POs,
 and this sieve serves that purpose.
-
-For the moment, the only available operation is to set a certain field
-to a given value. More operation modes may be implemented in the future.
 
 Sieve parameters for setting a field:
   - C{field:<name>:<value>}: set the named field to the given value
@@ -25,8 +22,25 @@ by default the new field is appended at the end of the header.
 If the field is present, using the option C{reorder} it can be moved within
 the header to match the order implied by C{after} and C{before}.
 
-Field value strings may contain %-directives, which are expanded to
-catalog-dependent substrings prior to setting the value. These are:
+Sieve parameters for setting comments:
+  - C{title}: set the title comment to the given value
+  - C{copyright}: set the copyright comment to the given value
+  - C{license}: set the license comment to the given value
+  - C{author}: set the author comment to the given value
+
+The C{author} parameter can be repeated to set several authors.
+All existing authors are removed before setting the new ones
+(regardless of the number of each),
+i.e. the new authors are I{not} appended to the old.
+
+Comment values are checked for some minimal consistency,
+e.g. author comments must contain email addresses,
+licence comments the word 'licence',
+etc.
+
+Value strings (both of fields and comments) may contain %-directives,
+which are expanded to catalog-dependent substrings prior to setting the value.
+These are:
   - C{%poname}: catalog name (equal to file name without C{.po} extension)
 
 If literal %-sign is needed, it can be escaped by entering C{%%}.
@@ -40,8 +54,10 @@ if its use would be ambiguous otherwise.
 import re
 import time
 
-from pology.misc.report import error, warning
+from pology.sieve import SieveError
+from pology.misc.report import report, warning
 from pology.misc.resolve import expand_vars
+from pology.misc.monitored import Monlist
 
 def setup_sieve (p):
 
@@ -86,6 +102,28 @@ def setup_sieve (p):
     "'%(after)s' and '%(before)s' parameters, reinsert it accordingly."
     % dict(after="after", before="before")
     )
+    p.add_param("title", unicode,
+                metavar="VALUE",
+                desc=
+    "Set title comment to the given value."
+    )
+    p.add_param("copyright", unicode,
+                metavar="VALUE",
+                desc=
+    "Set copyright comment to the given value."
+    )
+    p.add_param("license", unicode,
+                metavar="VALUE",
+                desc=
+    "Set license comment to the given value."
+    )
+    p.add_param("author", unicode, multival=True,
+                metavar="VALUE",
+                desc=
+    "Set author comment to the given value. "
+    "This parameter can be repeated to set several authors. "
+    "All existing authors are removed before setting the new ones."
+    )
 
 
 class Sieve (object):
@@ -97,22 +135,48 @@ class Sieve (object):
         for field_value_str in (params.field or []):
             field_value = field_value_str.split(":", 1)
             if len(field_value) != 2:
-                error("invalid specification of header field and value: %s"
-                      % field_value_str)
+                raise SieveError("invalid specification of header field "
+                                 "and value: %s" % field_value_str)
             self.fields_values.append(field_value)
         # Set fields in reverse, so that 'after' and 'before' parameters
         # are followed by the order of appearance of fields in command line.
         self.fields_values.reverse()
 
+        # Check validity of comment values.
+        if params.copyright is not None:
+            if not re.search("copyright", params.copyright):
+                raise SieveError("invalid value for copyright comment "
+                                 "(missing word 'copyright'?): %s"
+                                 % params.copyright)
+        if params.license is not None:
+            if not re.search("license", params.license):
+                raise SieveError("invalid value for license comment "
+                                 "(missing word 'license'?): %s"
+                                 % params.license)
+        for author in (params.author or []):
+            if not re.search("<.*@.*>", author):
+                raise SieveError("invalid value for author comment "
+                                 "(missing email address?): %s"
+                                 % author)
         self.p = params
 
 
     def process_header (self, hdr, cat):
 
         pvars = {"poname" : cat.name}
+
         for field, value in self.fields_values:
             if self.p.create or hdr.select_fields(field):
                 hdr.set_field(field, expand_vars(value, pvars),
                               after=self.p.after, before=self.p.before,
                               reorder=self.p.reorder)
+
+        if self.p.title is not None:
+            hdr.title = Monlist([expand_vars(self.p.title, pvars)])
+        if self.p.copyright is not None:
+            hdr.copyright = expand_vars(self.p.copyright, pvars)
+        if self.p.license is not None:
+            hdr.license = expand_vars(self.p.license, pvars)
+        if self.p.author is not None:
+            hdr.author = Monlist([expand_vars(x, pvars) for x in self.p.author])
 
