de0190560779c342dfc0cb0409d789e6240f8049
Karl Ove Hufthammer <karl@huftis.org>
2009-08-06 06:58:56 +0000

    Added check_rules files for Norwegian Nynorsk.
    svn path=/trunk/l10n-support/pology/; revision=1007703

---
+++ l10n/nn/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+# l10n.nn.__init__
+
+"""
+Language-specific tools for Norwegian Nynorsk.
+
+@author: Karl Ove Hufthammer <karl@huftis.org>
+@license: GPLv3
+"""
---
+++ l10n/nn/hook/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*
+
+"""
+Hooks specific for Norwegian Nynorsk.
+
+@author: Karl Ove Hufthammer <karl@huftis.org>
+@license: GPLv3
+"""
