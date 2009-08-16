f7a14050975e2789b5a568471b824e62e23a0d7f
Chusslove Illich <caslav.ilic@gmx.net>
2009-08-16 19:28:09 +0000

    Hook module for equipping headers.
    svn path=/trunk/l10n-support/pology/; revision=1012093

---
+++ hook/equip_header.py
@@ -0,0 +1,81 @@
+# -*- coding: UTF-8 -*-
+
+"""
+Equip catalog headers with extra information.
+
+@author: Chusslove Illich (Часлав Илић) <caslav.ilic@gmx.net>
+@license: GPLv3
+"""
+
+import os
+import re
+
+
+from pology.sieve.check_tp_kde import is_qt_cat, is_txt_cat, is_docbook_cat
+from pology.sieve.check_tp_kde import _get_catalog_project_subdir
+
+def equip_header_tp_kde (hdr, cat):
+    """
+    Equip headers in catalogs within KDE Translation Project [type F4B hook].
+
+    The following header fields are set:
+    - C{Language}: the language code of translation;
+            set only if the language can be determined
+    - C{X-Environment}: linguistic subset of the language of translation
+            (team choices on terminology, ortography...); set to C{kde}
+    - C{X-Accelerator-Marker}: accelerator marker character which may
+            be encountered in text
+    - C{X-Text-Markup}: text markups (e.g. Qt rich text, Docbook...) which
+            may be encountered in text, as keywords
+
+    For the hook to function properly, the local checkout of language catalogs
+    must match the repository structure up to a certain level.
+    See the documentation on L{check-tp-kde<sieve.check_tp_kde>} sieve for
+    details.
+    """
+
+    cname = cat.name
+    csubdir = _get_catalog_project_subdir(cat.filename)
+
+    pathels = os.path.abspath(cat.filename).split(os.path.sep)
+    lang_rx = re.compile(r"^[a-z]{2}(_[A-Z]{2}|@[a-z]+)?$")
+    lang = None
+    if len(pathels) >= 5 and pathels[-4] == "summit":
+        if lang_rx.search(pathels[-5]):
+            lang = pathels[-5]
+    elif len(pathels) >= 4:
+        if lang_rx.search(pathels[-4]):
+            lang = pathels[-4]
+
+    if is_txt_cat(cname, csubdir):
+        accmark = ""
+        mtypes = [""]
+    elif is_qt_cat(cname, csubdir):
+        accmark = "&"
+        mtypes = ["qtrich"]
+    elif is_docbook_cat(cname, csubdir):
+        accmark = ""
+        mtypes = ["docbook4"]
+    else: # default to native KDE4 catalog
+        accmark = "&"
+        mtypes = ["kde4"]
+
+    # Special handling for particular catalogs.
+    if cname == "desktop_kdebase":
+        accmark = "&"
+
+    fvs = [
+        (u"Language", unicode(lang), "Language-Team"),
+        (u"X-Environment", u"kde", None),
+        (u"X-Accelerator-Marker", unicode(accmark), None),
+        (u"X-Text-Markup", u", ".join(mtypes), None),
+    ]
+    for fnam, fval, fnamaft in fvs:
+        if fval is None:
+            continue
+        if len(hdr.select_fields(fnam)) > 1:
+            hdr.remove_field(fnam)
+        hdr.set_field(fnam, fval, after=fnamaft)
+
+    return 0
+
--- sieve/equip_header_tp_kde.py
+++ sieve/equip_header_tp_kde.py
@@ -3,30 +3,13 @@
 """
 Equip catalog headers within KDE Translation Project with extra information.
 
-The following header fields are set:
-  - C{Language}: the language code of translation;
-        set only if the language can be determined
-  - C{X-Environment}: linguistic subset of the language of translation
-        (team choices on terminology, ortography...); set to C{kde}
-  - C{X-Accelerator-Marker}: accelerator marker character which may
-        be encountered in text
-  - C{X-Text-Markup}: text markups (e.g. Qt rich text, Docbook...) which
-        may be encountered in text, as keywords
-
-For the sieve to function properly, the local checkout of language catalogs
-must match the repository structure up to a certain level.
-See the documentation on L{check-tp-kde<sieve.check_tp_kde>} sieve for
-details.
+This sieve applies L{hook.equip_header.equip_header_tp_kde} to catalog headers.
 
 @author: Chusslove Illich (Часлав Илић) <caslav.ilic@gmx.net>
 @license: GPLv3
 """
 
-import os
-import re
-
-from pology.sieve.check_tp_kde import is_qt_cat, is_txt_cat, is_docbook_cat
-from pology.sieve.check_tp_kde import _get_catalog_project_subdir
+from pology.hook.equip_header import equip_header_tp_kde
 
 
 def setup_sieve (p):
@@ -46,46 +29,5 @@ class Sieve (object):
 
     def process_header (self, hdr, cat):
 
-        cname = cat.name
-        csubdir = _get_catalog_project_subdir(cat.filename)
-
-        pathels = os.path.abspath(cat.filename).split(os.path.sep)
-        lang_rx = re.compile(r"^[a-z]{2}(_[A-Z]{2}|@[a-z]+)?$")
-        lang = None
-        if len(pathels) >= 5 and pathels[-4] == "summit":
-            if lang_rx.search(pathels[-5]):
-                lang = pathels[-5]
-        elif len(pathels) >= 4:
-            if lang_rx.search(pathels[-4]):
-                lang = pathels[-4]
-
-        if is_txt_cat(cname, csubdir):
-            accmark = ""
-            mtypes = [""]
-        elif is_qt_cat(cname, csubdir):
-            accmark = "&"
-            mtypes = ["qtrich"]
-        elif is_docbook_cat(cname, csubdir):
-            accmark = ""
-            mtypes = ["docbook4"]
-        else: # default to native KDE4 catalog
-            accmark = "&"
-            mtypes = ["kde4"]
-
-        # Special handling for particular catalogs.
-        if cname == "desktop_kdebase":
-            accmark = "&"
-
-        fvs = [
-            (u"Language", unicode(lang), "Language-Team"),
-            (u"X-Environment", u"kde", None),
-            (u"X-Accelerator-Marker", unicode(accmark), None),
-            (u"X-Text-Markup", u", ".join(mtypes), None),
-        ]
-        for fnam, fval, fnamaft in fvs:
-            if fval is None:
-                continue
-            if len(hdr.select_fields(fnam)) > 1:
-                hdr.remove_field(fnam)
-            hdr.set_field(fnam, fval, after=fnamaft)
+        equip_header_tp_kde(hdr, cat)
 
