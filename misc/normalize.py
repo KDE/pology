f0eac4b43570fdbab879a7787f8b79f10ce89334
Chusslove Illich <caslav.ilic@gmx.net>
2009-09-09 22:30:24 +0000

    New module: string normalization functions.
    svn path=/trunk/l10n-support/pology/; revision=1021738

---
+++ misc/normalize.py
@@ -0,0 +1,133 @@
+# -*- coding: UTF-8 -*-
+
+"""
+Various normalizations for strings.
+
+@author: Chusslove Illich (Часлав Илић) <caslav.ilic@gmx.net>
+@license: GPLv3
+"""
+
+_wsseq_rx = re.compile(r"[ \t\n]+", re.U)
+
+def simplify (s):
+    """
+    Simplify ASCII whitespace in the string.
+
+    All leading and trailing ASCII whitespace are removed,
+    all inner ASCII whitespace sequences are replaced with space.
+
+    @param s: string to normalize
+    @type s: string
+
+    @returns: normalized string
+    @rtype: string
+    """
+
+    return _wsseq_rx.sub(" ", s.strip())
+
+
+_uwsseq_rx = re.compile(r"\s+", re.U)
+
+def usimplify (s):
+    """
+    Simplify whitespace in the string.
+
+    Like L{simplify}, but takes into account all whitespace defined by Unicode.
+
+    @param s: string to normalize
+    @type s: string
+
+    @returns: normalized string
+    @rtype: string
+    """
+
+    return _uwsseq_rx.sub(" ", s.strip())
+
+
+def shrink (s):
+    """
+    Remove all whitespace from the string.
+
+    @param s: string to normalize
+    @type s: string
+
+    @returns: normalized string
+    @rtype: string
+    """
+
+    return _uwsseq_rx.sub("", s)
+
+
+def tighten (s):
+    """
+    Remove all whitespace and lowercase the string.
+
+    @param s: string to normalize
+    @type s: string
+
+    @returns: normalized string
+    @rtype: string
+    """
+
+    return _uwsseq_rx.sub("", s.lower())
+
+
+_non_ascii_ident_rx = re.compile(r"[^a-z0-9_]", re.U|re.I)
+
+def identify (s):
+    """
+    Construct an uniform-case ASCII-identifier out of the string.
+
+    ASCII-identifier is constructed in the following order:
+      - the string is lowercased
+      - every character that is neither an ASCII alphanumeric nor
+        the underscore is removed
+      - if the string starts with a digit, underscore is prepended
+
+    @param s: string to normalize
+    @type s: string
+
+    @returns: normalized string
+    @rtype: string
+    """
+
+    ns = s
+
+    # Lowercase.
+    ns = ns.lower()
+
+    # Remove non-identifier chars.
+    ns = _non_ascii_ident_rx.sub("", ns) 
+
+    # Prefix with underscore if first char is digit.
+    if ns[0:1].isdigit():
+        ns = "_" + ns
+
+    return ns
+
+
+def xentitize (s):
+    """
+    Replace characters having default XML entities with the entities.
+
+    The replacements are:
+      - C{&amp;} for ampersand
+      - C{&lt} and C{&gt;} for less-than and greater-then signs
+      - C{&apos;} and C{&quot;} for ASCII single and double quotes
+
+    @param s: string to normalize
+    @type s: string
+
+    @returns: normalized string
+    @rtype: string
+    """
+
+    ns = s
+    ns = ns.replace("&", "&amp;") # must come first
+    ns = ns.replace("<", "&lt;")
+    ns = ns.replace(">", "&gt;")
+    ns = ns.replace("'", "&apos;")
+    ns = ns.replace('"', "&quot;")
+
+    return ns
+
