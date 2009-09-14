e5a4b2c07a80819bbecab06444715ff7ac89c9bc
Chusslove Illich <caslav.ilic@gmx.net>
2009-09-14 22:58:38 +0000

    Converted to new style. Added parameters strip and branch.
    svn path=/trunk/l10n-support/pology/; revision=1023566

--- sieve/tag_untranslated.py
+++ sieve/tag_untranslated.py
@@ -1,38 +1,100 @@
 # -*- coding: UTF-8 -*-
 
+"""
+Tag untranslated messages.
+
+Some people like to translate PO files with ordinary text editor,
+which may provide no more support for editing PO files other
+than e.g. syntax highlighting.
+In such a scenario, this sieve can be used to equip untranslated messages
+with C{untranslated} flag, for easy lookup in the editor.
+E.g. to jump through all incomplete messages (fuzzy and untranslated),
+translator can issue C{, fuzzy|, untranslated} regular expression
+in editor's search facility.
+
+Note that C{untranslated} flags, being custom, will be lost when the PO file
+is merged with the template next time. This is intentional: the only purpose
+of the flag is for immediate editing, and the translator may forget to remove
+some. There is no reason for the flags to persist in that case. Also, if
+the flag is left in after the message has been translated, the subsequent
+run of this sieve will remove the flag.
+
+Sieve options:
+  - C{branch:<branch_id>}: consider only messages from this branch (summit)
+  - C{strip}: instead of adding, strip any C{untranslated} flags
+
+For L{summited<scripts.posummit>} catalogs, the C{branch} option is used to
+restrict modifications to messages from the given branch only.
+Several branch IDs may be given as a comma-separated list.
+
+@author: Chusslove Illich (Часлав Илић) <caslav.ilic@gmx.net>
+@license: GPLv3
+"""
+
+from pology.misc.report import report
+from pology.misc.comments import parse_summit_branches
+
+
+def setup_sieve (p):
+
+    p.set_desc(
+    "Tag all untranslated messages."
+    )
+
+    p.add_param("strip", bool,
+                desc=
+    "Remove tags from untranslated messages."
+    )
+    p.add_param("branch", unicode, seplist=True,
+                metavar="BRANCH",
+                desc=
+    "In summited catalogs, consider only messages belonging to given branch. "
+    "Several branches can be given as comma-separated list."
+    )
+
+
+_flag_untranslated = u"untranslated"
+
 class Sieve (object):
-    """Add flag "untranslated" to untranslated messages."""
 
-    def __init__ (self, options):
+    def __init__ (self, params):
 
-        self.nmatch = 0
+        self.strip = params.strip
+        self.branches = set(params.branch or [])
 
-        # Indicators to the caller:
-        # - monitor to avoid unnecessary reformatting when unfuzzied
-        self.caller_monitored = True
+        self.ntagged = 0
+        self.ncleared = 0
 
 
     def process (self, msg, cat):
-
-        flag_untranslated = u"untranslated"
 
         # Skip obsolete messages.
         if msg.obsolete:
             return
 
-        # Add flag if untranslated.
-        if msg.untranslated:
-            msg.flag.add(flag_untranslated)
-            self.nmatch += 1
-            msg.modcount = 1 # in case of non-monitored messages
+        # Summit: if branches were given, considered the message for
+        # tagging based on whether it belongs to any of the given branches.
+        may_tag = True
+        if self.branches:
+            msg_branches = parse_summit_branches(msg)
+            if not set.intersection(self.branches, msg_branches):
+               may_tag = False
 
-        # Remove flag if already there, but message is no longer untranslated.
-        elif flag_untranslated in msg.flag and not msg.untranslated:
-            msg.flag.remove(flag_untranslated)
-            msg.modcount = 1 # in case of non-monitored messages
+        if not self.strip and may_tag and msg.untranslated:
+            if _flag_untranslated not in msg.flag:
+                msg.flag.add(_flag_untranslated)
+                self.ntagged += 1
+        else:
+            if _flag_untranslated in msg.flag:
+                msg.flag.remove(_flag_untranslated)
+                self.ncleared += 1
 
 
     def finalize (self):
 
-        if self.nmatch > 0:
-            print "Total untranslated tagged: %d" % (self.nmatch,)
+        if self.ntagged > 0:
+            report("Total untranslated messages tagged: %d" % self.ntagged)
+        if self.ncleared > 0:
+            report("Total untranslated messages cleared of tags: %d"
+                   % self.ncleared)
+
