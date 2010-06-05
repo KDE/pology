612d9df53e38265123a0077779760dbb5fdebe95
Chusslove Illich <caslav.ilic@gmx.net>
2010-06-05 13:06:24 +0000

    Different modes for no-break hyphen insertion.
    svn path=/trunk/l10n-support/pology/; revision=1134837

--- l10n/sr/hook/nobr.py
+++ l10n/sr/hook/nobr.py
@@ -19,12 +19,17 @@ from pology.l10n.sr.hook.wconv import ctol
 
 nobrhyp_char = u"\u2011"
 
-def to_nobr_hyphens (wchars="", unsafe=False):
+def to_nobr_hyphens (mode=0, wchars="", unsafe=False):
     """
-    Replace some ordinary with no-break hyphens [hook factory].
+    Replace some ordinary hyphens with no-break hyphens [hook factory].
 
-    The ordinary hyphen is replaced if it is in between two letters,
-    and either preceded or followed by at most four letters.
+    An ordinary hyphen is replaced in one of the following modes,
+    as given by the C{mode} parameter:
+      - 0: if the hyphen is in between two letters, and either preceded
+            or followed by at most four letters
+      - 1: if the hyphen is in between two letters and followed by
+            exactly one letter
+
     Using the C{wchars} parameter, some extra characters other than letters
     can be treated as equal to letters.
 
@@ -34,6 +39,8 @@ def to_nobr_hyphens (wchars="", unsafe=False):
     This can be relaxed by setting C{unsafe} to C{True},
     when all letters are treated equally.
 
+    @param mode: replacement mode
+    @type mode: int
     @param wchars: extra characters to consider parts of the word
     @type wchars: string
     @param unsafe: whether to replace hyphen even if no Cyrillic letters nearby
@@ -45,11 +52,18 @@ def to_nobr_hyphens (wchars="", unsafe=False):
 
     wchars = wchars.replace("-", "") # just in case
 
-    # Catching possible replacement by text before hyphen.
-    nobrhyp_be_rx = re.compile(ur"\b(\w{1,4})(-)([\w%s])" % wchars, re.U)
-
-    # Catching possible replacement by text after hyphen.
-    nobrhyp_af_rx = re.compile(ur"([\w%s])(-)(\w{1,4})\b" % wchars, re.U)
+    nobrhyp_rxstrs = []
+    if mode == 0:
+        # Catching possible replacement by text before hyphen.
+        nobrhyp_rxstrs.append(ur"\b(\w{1,4})(-)([\w%s])" % wchars)
+        # Catching possible replacement by text after hyphen.
+        nobrhyp_rxstrs.append(ur"([\w%s])(-)(\w{1,4})\b" % wchars)
+    elif mode == 1:
+        # Catching possible replacement by text after hyphen.
+        nobrhyp_rxstrs.append(ur"([\w%s])(-)(\w{1})\b" % wchars)
+    else:
+        raise StandardError("Unknown hyphen replacement mode %(mode)s." % mode)
+    nobrhyp_rxs = [re.compile(x, re.U) for x in nobrhyp_rxstrs]
 
     # Function to produce replacement for matched pattern.
     if not unsafe:
@@ -75,8 +89,8 @@ def to_nobr_hyphens (wchars="", unsafe=False):
         # that the regexes will not catch in one pass (e.g. аб-вг-дђ).
         while True:
             text_prev = text
-            text = nobrhyp_be_rx.sub(nobrhyp_repl, text)
-            text = nobrhyp_af_rx.sub(nobrhyp_repl, text)
+            for nobrhyp_rx in nobrhyp_rxs:
+                text = nobrhyp_rx.sub(nobrhyp_repl, text)
             if text_prev == text:
                 break
 
