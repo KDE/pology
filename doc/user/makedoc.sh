49bc9e90196caec88daede69ab18642f5eb610de
Chusslove Illich <caslav.ilic@gmx.net>
2010-09-04 09:18:23 +0000

    Remove title= attributes from sectioning classes.
    svn path=/trunk/l10n-support/pology/; revision=1171539

--- doc/user/makedoc.sh
+++ doc/user/makedoc.sh
@@ -15,6 +15,9 @@ ln -s ../html-data html/data
 xsltproc --xinclude html-mono.xsl $topdbk >html/index-mono.html
 xsltproc --xinclude html-chapters.xsl $topdbk
 tidy -q --show-warnings no -utf8 -w 0 -m html/*.html; test -z
+# # Remove title= attributes to sectioning classes,
+# because they cause a tooltip to be shown wherever the pointer is.
+perl -pi -e 's/(<div[^>]*?class="(abstract|article|book|chapter|sect)[^>]*?) *title=".*?"/\1/' html/*.html
 
 svn status html/ | grep '^!' | sed 's/.//' | xargs -r svn rm
 svn status html/ | grep '^?' | sed 's/.//' | xargs -r svn add
