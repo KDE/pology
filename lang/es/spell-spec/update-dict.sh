aefa6e3cfe3bc17684bb99ff77ad7030622456bf
Francisco Javier Viñal <fjvinal@gmail.com>
2010-09-15 18:07:23 +0000

    Update of Spanish rules and dictionary for pology. See CHANGELOG for details (in Spanish)
    svn path=/trunk/l10n-support/pology/; revision=1175730

--- lang/es/spell-spec/update-dict.sh
+++ lang/es/spell-spec/update-dict.sh
@@ -4,4 +4,4 @@
  
 echo "personal_ws-1.1 es 9999 utf-8" > ../spell/dict.aspell 
 cat *.aspell >> ../spell/dict.aspell
-../../../../scripts/organizeDict.py ../spell/dict.aspell
+../../../scripts/organizeDict.py ../spell/dict.aspell
