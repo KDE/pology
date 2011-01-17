f9884b345fd3cd83db2892793041a16cebac1330
Chusslove Illich <caslav.ilic@gmx.net>
2011-01-17 11:55:40 +0000

    Allow cp to clobber files, to avoid superfluous warnings.
    svn path=/trunk/l10n-support/pology/; revision=1215047

--- www/updatewww.sh
+++ www/updatewww.sh
@@ -2,8 +2,8 @@
 
 cd $(dirname $0)
 
-cp -aL base tmpwww
-cp -aL ../doc-html/* tmpwww/doc/
+cp -aLf base tmpwww
+cp -aLf ../doc-html/* tmpwww/doc/
 # Expects www-pology entry in SSH config.
 rsync -rav --delete --cvs-exclude tmpwww/ www-pology:pology.nedohodnik.net/
 rm -rf tmpwww
