4ed305b70410453d1cdf994f986daeb7bf415ad0
Francisco Javier Viñal <fjvinal@gmail.com>
2011-01-23 08:50:12 +0000

    Update of Spanish rules and scripts for pology.
    svn path=/trunk/l10n-support/pology/; revision=1216415

--- pology/lang/es/remove_subs.py
+++ pology/lang/es/remove_subs.py
@@ -98,3 +98,32 @@ def remove_paired_parameters (msg, cat):
 
     return 0
 
+
+_auto_cmnt_tag_rx = re.compile("^\s*Tag:\s*(%s)\s*$" % "|".join("""
+    trans_comment literallayout option programlisting othercredit author email holder
+    surname personname affiliation address sect1 chapter chapterinfo date command option
+    refentrytitle refentryinfo refname synopsis literal varname term glossterm
+    filename envar userinput cmdsynopsis
+""".split()), re.U|re.I)
+
+def remove_tags_without_translation (msg, cat):
+    """
+    Remove all paragraph that belong to contexts that do not 
+    have need of translation.[type F4A hook].
+
+    @return: number of errors
+    """
+    if msg.msgctxt in ("EMAIL OF TRANSLATORS", "NAME OF TRANSLATORS", "ROLES OF TRANSLATORS"):
+        msg.msgid = ""
+        msg.msgid_plural = ""
+        msg.msgstr = ""
+	return 0
+	
+    # Avoid specially tagged messages.
+    for auto_cmnt in msg.auto_comment:
+        if _auto_cmnt_tag_rx.search(auto_cmnt):
+	    msg.msgid = ""
+	    msg.msgid_plural = ""
+	    msg.msgstr = ""
+    
+    return 0
