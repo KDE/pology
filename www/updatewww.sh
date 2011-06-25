6dfa04368c27d77711b55784a00de0b46a7dab90
Chusslove Illich <caslav.ilic@gmx.net>
2011-06-25 13:41:01 +0000

    Make sure everything is updated before syncing with web site.
    svn path=/trunk/l10n-support/pology/; revision=1238306

--- www/updatewww.sh
+++ www/updatewww.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 
 cd $(dirname $0)
+srcdir=..
 
+echo "Copying base files..."
 cp -aLf base tmpwww
-cp -aLf ../doc-html/* tmpwww/doc/
+
+echo "Building local documentation:"
+rm -rf $srcdir/doc-html
+echo "- user manual..."
+$srcdir/doc/user/local.sh build
+echo "- language support manuals..."
+for local in $srcdir/lang/*/doc/local.sh; do
+    $local build || exit 1
+done
+echo "- API documentation..."
+$srcdir/doc/api/local.sh build
+cp -aL $srcdir/doc-html/* tmpwww/doc/
+
+echo "Syncing with web site..."
 # Expects www-pology entry in SSH config.
 rsync -rav --delete --cvs-exclude tmpwww/ www-pology:pology.nedohodnik.net/
 rm -rf tmpwww
+
+echo "All done."
