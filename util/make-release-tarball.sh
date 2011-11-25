193ea91c3936fae246334f259ff5b9d5d73e09e0
Chusslove Illich <caslav.ilic@gmx.net>
2011-11-25 11:20:20 +0000

    Some reworking in version handling.
    svn path=/trunk/l10n-support/pology/; revision=1265752

--- util/make-release-tarball.sh
+++ util/make-release-tarball.sh
@@ -7,41 +7,25 @@
 # Can be run from any directory.
 
 version=$1
+
+cmddir=`dirname $0`
+
 if test -z "$version"; then
-    echo "Version number not given."
-    exit 1
+    version=$(cat $cmddir/../VERSION)
+    echo "Version number: $version (from VERSION)"
+else
+    echo $version >VERSION
+    echo "Version number: $version (manually given)"
 fi
-
-#branch=`echo $version | sed -r 's/\.[0-9]+$//'`
-#if test $branch = $version; then
-#    echo "Malformed version string '$version'."
-#    echo 1
-#fi
-
-date=`date -I`
 
 pkgname=pology-$version
 
 echo "Creating tarball directory..."
-cmddir=`dirname $0`
 tdir=/tmp/$pkgname
 rm -rf $tdir
 cp -r $cmddir/.. $tdir
 # All following actions happen in tarball directory.
 cd $tdir
-
-echo "Updating version information in files..."
-echo $version >VERSION
-sed -r '/^\s*Version\s+\S+-dev:\s*$/d' NEWS >NEWS.tail
-if cmp -s NEWS NEWS.tail; then
-    echo "Malformed NEWS file."
-    echo 1
-fi
-cat >NEWS.head <<EOF
-Version $version, $date:
-EOF
-cat NEWS.head NEWS.tail >NEWS
-rm NEWS.head NEWS.tail
 
 echo "Removing non-distributed files..."
 find -iname \.svn | xargs rm -rf
