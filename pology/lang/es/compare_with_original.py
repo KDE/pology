c0be4b8554fb12c51e2225aac2d57ae9bcb34c5e
Francisco Javier Viñal <fjvinal@gmail.com>
2012-02-07 22:36:43 +0000

    Update of Spanish rules, scripts an dictionaries for pology.
    svn path=/trunk/l10n-support/pology/; revision=1278664

--- pology/lang/es/compare_with_original.py
+++ pology/lang/es/compare_with_original.py
@@ -77,6 +77,7 @@ def test_if_very_short_translation (msg, cat):
 
 _valid_word = re.compile("(?u)^[^\W\d_]+$")
 _capital_word = re.compile("(?u)^[A-Z]+$")
+
 def test_if_not_translated (msg, cat):
     """
     Compare the translation with the original text, testing if the paragraph is 
@@ -161,3 +162,76 @@ def test_paired_tabs (msg, cat):
             return [("msgstr", 0, [(0, 0, "Faltan tabuladores en la traducción")])]
 
     return []
+    
+
+_ent_function = re.compile("\w[\w\d]*(\-[\w\d]+)*\(\)")
+
+def test_paired_functions (msg, cat):
+    """
+    Compare functions names between original and translated text. 
+    
+    [type V4A hook].
+    @return: parts
+    """
+    for i in range(len(msg.msgstr)):
+        if i > 0:
+             msgid = msg.msgid_plural
+        else:
+             msgid = msg.msgid
+
+        function_orig = _ent_function.findall(msgid)
+        function_trans = _ent_function.findall(msg.msgstr[i])
+    
+        if function_orig != function_trans:
+            return [("msgstr", 0, [(0, 0, "Nombres de función distintos en la traducción")])]
+ 
+    return []
+    
+    
+_ent_parameter = re.compile("\-\-\w[\w\d]*(\-[\w\d]+)*")
+
+def test_paired_parameters (msg, cat):
+    """
+    Compare parameters names between original and translated text. 
+    
+    [type V4A hook].
+    @return: parts
+    """
+    for i in range(len(msg.msgstr)):
+        if i > 0:
+             msgid = msg.msgid_plural
+        else:
+             msgid = msg.msgid
+
+        parameter_orig = _ent_parameter.findall(msgid)
+        parameter_trans = _ent_parameter.findall(msg.msgstr[i])
+    
+        if parameter_orig != parameter_trans:
+            return [("msgstr", 0, [(0, 0, "Nombres de parámetros distintos en la traducción")])]
+ 
+    return []
+
+    
+_ent_number = re.compile("[^\%][\dXx]+([\.\,\:\/][\dXx]+)+")
+
+def test_paired_numbers (msg, cat):
+    """
+    Compare numbers and dates between original and translated text. 
+    
+    [type V4A hook].
+    @return: parts
+    """
+    for i in range(len(msg.msgstr)):
+        if i > 0:
+             msgid = msg.msgid_plural
+        else:
+             msgid = msg.msgid
+
+        number_orig = _ent_number.findall(msgid)
+        number_trans = _ent_number.findall(msg.msgstr[i])
+    
+        if number_orig != number_trans:
+            return [("msgstr", 0, [(0, 0, "Valores de números distintos en la traducción")])]
+ 
+    return []
+
