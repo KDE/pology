ce4ee6fe77ad3de9d77f4f1706f3ccfe95592d10
Francisco Javier Viñal <fjvinal@gmail.com>
2013-02-16 20:15:52 +0000

    Update of Spanish rules and scripts for pology. CCMAIL:traducciones@rociogallego.com
    svn path=/trunk/l10n-support/pology/; revision=1340471

--- lang/es/scripts/revisar-traducciones-con-pology.sh
+++ lang/es/scripts/revisar-traducciones-con-pology.sh
@@ -4,17 +4,42 @@ RAIZ=~/svnroot/
 RAMA=trunk
 SALIDA=~/svnroot/revisiones/
 RUTA_LT=/usr/share/languagetool
+IDIOMA=es
 
 case $1 in
     "reglas")
+	for i in $@;
+	do
+	    if [[ $i =~ ^[0-3]+$ ]];
+	    then
+		NIVEL=$NIVEL$i
+	    elif [[ $i =~ ^(bp|cm|ff|gd|gr|gp|gk|pw|pb|pe|sp|tr|ty|ui)$ ]];
+	    then
+		if [[ $REGLA == "" ]];
+		then
+		    REGLA=$i
+		else
+		    REGLA=$REGLA'|'$i
+		fi
+	    fi
+	done
+	if [[ $NIVEL == "" ]];
+	then
+	    NIVEL='0123'
+	fi
+	if [[ $REGLA == "" ]];
+	then
+	    REGLA='..'
+	fi
+	echo "Niveles seleccionados:" $NIVEL "Reglas seleccionadas:" $REGLA
 	OPCION=check-rules
-	PARAMETROS='-s lang:es -s accel:& -s env:kde';;
+	PARAMETROS='-s lang:'$IDIOMA' -s accel:& -s env:kde -s rulerx:^('$REGLA')\-['$NIVEL']\-';;
     "ortografía")
 	OPCION=check-spell
-	PARAMETROS='-s lang:es -s accel:& -s case -s skip:[A-Z]*[A-Z]|0-9|\W';;
+	PARAMETROS='-s lang:'$IDIOMA' -s accel:& -s case -s skip:[A-Z]+';;
     "gramática")
 	OPCION=check-grammar
-	PARAMETROS='-s lang:es -s accel:&'
+	PARAMETROS='-s lang:'$IDIOMA' -s accel:&'
 	java -jar $RUTA_LT/LanguageToolGUI.jar &
 	sleep 5;;
     "kde4")
@@ -23,18 +48,18 @@ case $1 in
     "tp-kde")
 	OPCION=check-tp-kde
 	PARAMETROS="";;
-    *)	
+    *)
 	echo 'El parámetro debe ser: reglas|ortografía|gramática|kde4|tp-kde';
 	exit 1;;
 esac
 
 for TIPO in 'messages' 'docmessages'; do
-    ORIGEN=$RAIZ/$RAMA/l10n-kde4/es/$TIPO
+    ORIGEN=$RAIZ/$RAMA/l10n-kde4/$IDIOMA/$TIPO
     DESTINO=$SALIDA/$1/$TIPO
     mkdir -p $DESTINO
     for PAQUETE in $ORIGEN/*; do
 	if [ -d $PAQUETE ]; then
-	    $PROGRAMA '--skip-obsolete' $OPCION $PARAMETROS $PAQUETE > $DESTINO/$(basename $PAQUETE)
+	    $PROGRAMA '-bR' $OPCION $PARAMETROS $PAQUETE > $DESTINO/$(basename $PAQUETE)
 	fi
     done
 done
