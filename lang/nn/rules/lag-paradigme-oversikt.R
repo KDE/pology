e0b1dc27da834bdf900969b3b2c54dcdba2e25ea
Karl Ove Hufthammer <karl@huftis.org>
2018-08-05 11:59:51 +0000

    Scripts and data file for conjugation alternatives
    The script uses data files from Norsk ordbank (NO)
    (https://www.nb.no/sprakbanken/show?serial=oai%3Anb.no%3Asbr-41&lang=en)
    to create a CSV data file of all combinations of
    conjugation paradigms that are present in NO. This
    file will be manually edited to select the preferred
    paradigme (if any) for each combination. Later,
    scripts will be added to extract lists of ‘forbidden’
    conjugations based on the CSV file.
    
    svn path=/trunk/l10n-support/pology/; revision=1521850

---
+++ lang/nn/rules/lag-paradigme-oversikt.R
@@ -0,0 +1,106 @@
+#!/usr/bin/Rscript --vanilla
+#
+# Lag oversikt over kva kombinasjonar av bøyingar
+# som eksisterer i Norsk ordbank.
+#
+# For kvar kombinasjon vel me éi føretrekt bøying,
+# dersom dette gjev meining (det gjer det for eksempel
+# ikkje for ord som berre har valfritt kjønn).
+#
+# Brukar òg ordfrekvensinformasjon frå KDE-omsettinga,
+# for å få betre ordeksempel (funkar ikkje perfekt,
+# men er nyttig).
+
+# Ymse pakkar for datahandsaming
+library(tidyverse)
+
+# Mappe som inneheld filene til Norsk ordbank
+# og frekvensinformasjon frå KDE-omsettinga
+mappe_ordbank = "~/utvikling/ordbanken"
+mappe_frek = "~/utvikling/kde/trunk/l10n-support/nn/skript/frekvensoversikt"
+
+# Les ordbankfiler og fjern unødvendige kolonnar
+les_fil = function(filnamn) {
+  d = read_tsv(paste0(mappe_ordbank, "/", filnamn))
+  d$X1 = NULL           # Fjern kolonne med linjenummer
+  d$`'NOB_2012'` = NULL # Fjern uinteressant kolonne
+  d
+}
+
+# Les inn alle datafilene
+d_lemma = les_fil("lemma_nn.txt")
+d_fullform = les_fil("fullformsliste_nn.txt")
+d_lemmaparadigme = les_fil("lemma_paradigme_nn.txt")
+d_paradigme = les_fil("paradigme_boying_nn.txt")
+d_frek = read_table(paste0(mappe_frek, "/", "frekvens-nn.dat"),
+                    col_names=c("frek","OPPSLAG"), col_type="nc")
+
+# Legg til bruksfrekvensinfo for kvar fullform,
+# og oppsummer til slutt på lemmanivå
+# (vil vera misvisande for homograf, men ikkje noko stort problem)
+d_ffrek = d_fullform %>% 
+  select(LEMMA_ID, OPPSLAG) %>% 
+  distinct %>% # Vil ikkje telja frekvensinfo for duplikatbøyingar fleire gongar
+  left_join(d_frek, by="OPPSLAG")
+d_lfrek = d_ffrek %>% 
+  group_by(LEMMA_ID) %>% 
+  summarise(frek = sum(frek, na.rm = TRUE)) %>% 
+  left_join(d_lemma)
+
+# d_lemmaparadigme skal i utgangspunktet innehalda alle
+# bøyingsparadigma til dei ulike lemmaa (gjer det ikkje heilt,
+# men det er vel ein feil som skal rettast?), så me
+# brukar denne for å finna paradigmekomboar
+
+# Paradigma står i vilkårleg rekkjefølgje innanfor lemma,
+# så me vil sjå vekk frå rekkjefølgja
+d_lp = d_lemmaparadigme %>% 
+  arrange(LEMMA_ID, PARADIGME_ID)
+
+# Samandrag over paradigme for kvart lemma
+d_psam_alle = d_lp %>% 
+  group_by(LEMMA_ID) %>% 
+  summarise(par_tekst = str_c(PARADIGME_ID, collapse=","),
+            n_par = n())
+
+# Sjå berre på tilfelle der me har valfridom i bøying
+d_psam = d_psam_alle %>% 
+  filter(n_par > 1)
+
+# Legg til frekvensinfo og hent ut
+# det mest frekvente lemmaet for
+# kvar paradigmekombo
+d_psam_pop = d_psam %>%
+  left_join(d_lfrek) %>% 
+  arrange(par_tekst, desc(frek)) %>% 
+  distinct(par_tekst, .keep_all = TRUE) %>%  # Berre første/mest populære lemma
+  arrange(desc(frek)) # Sorter paradigmekomboar etter frekvens
+
+# # Nokre eksempel
+# d_fullform %>% filter(LEMMA_ID==d_psam_pop$LEMMA_ID[1])
+# d_fullform %>% filter(LEMMA_ID==d_psam_pop$LEMMA_ID[2])
+# d_fullform %>% filter(LEMMA_ID==d_psam_pop$LEMMA_ID[3])
+
+# Lagra fil for manuell redigering
+# Treng berre laga oversikt for ord
+# som faktisk er brukte i omsettingane
+d_utdata = d_psam_pop %>% 
+  filter(frek > 0) %>% 
+  select(par_tekst, LEMMA_ID, GRUNNFORM)
+  
+# Legg til ekstrakolonnar
+# (Brukar NA i staden for "" på grunn av seinare eksport)
+d_utdata$val = NA       # Paradigmet me vel for denne typen ord
+d_utdata$kommentar = NA # Kommentar til valet/paradigmekomboen
+
+# Lagra fila for manuell redigering
+#
+# Gjer det nøyaktig slik (og brukar for eksempel ikkje write_csv())
+# for at fila skal kunna redigerast med LibreOffice Calc utan at
+# det vert gjort formateringsendringar. Bruk følgjande innstillingar
+# ved import:
+#
+#   Skild med: Komma (ingenting anna)
+#   Formater sitatfelt som tekst: ja (kryssa av)
+#
+write.csv(d_utdata, "paradigme-komboar.csv", row.names = FALSE, na = "")
