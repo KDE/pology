3e05096a14b82fe1234f39e1b6142b62a43e0566
Alexander Potashev <aspotashev@gmail.com>
2019-10-06 09:09:11 +0300

    ru: Add sieve ru:fill-kstars-object-names

---
+++ lang/ru/sieve/fill_kstars_object_names.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python2
+# coding: utf8
+
+"""
+Fill in some comets' names that follow specific patterns.
+Run against kstars.po.
+
+Sieve has no options.
+
+@author: Alexander Potashev <aspotashev@gmail.com>
+@license: GPLv3
+"""
+
+from pology import _, n_
+from pology.report import report
+from pology.msgreport import report_msg_content
+import os
+import re
+
+
+def setup_sieve (p):
+
+    p.set_desc(_("@info sieve discription",
+    "Fill in some comets' names that follow specific patterns."
+    ))
+
+
+class Sieve (object):
+
+    def __init__ (self, params):
+        pass
+        # Other dates should have the following format: (yyyy-mm-dd)
+        self.date_re = re.compile("^[0-9][0-9][0-9][0-9]\-[0-9][0-9]\-[0-9][0-9]$")
+
+    def translate (self, msg):
+        if msg.msgctxt == "Asteroid name (optional)" and re.match(r'\([0-9]{4} [A-Z]{2}[0-9]{1,3}\)', msg.msgid):
+            return msg.msgid
+        if msg.msgctxt == 'Comet name (optional)':
+            m = re.match(r'([CP]/[0-9]* [A-Z0-9\-]{2,5}) \(([a-zA-Z ]*)\)', msg.msgid)
+            if m:
+                code = m.group(1)
+                name = m.group(2)
+
+                tr_name = {
+                    u'Great comet': u'Большая комета',
+                    u'PANSTARRS': u'Pan-STARRS',
+                    u'LINEAR': u'LINEAR',
+                    u'Lemmon': u'обзор Маунт-Леммон',
+                    u'NEOWISE': u'NEOWISE',
+                    u'Catalina': u'обзор Каталина',
+                    u'Borisov': u'Борисов',
+                    u'Messier': u'Мессье',
+                    u'Pons': u'Понс',
+                    u'Гершель': u'Гершель',
+                    u'Olbers': u'Ольберс',
+                    u'Winnecke': u'Виннеке',
+                    u'WISE': u'WISE',
+                    u'Tempel': u'Темпель',
+                    u'STEREO': u'STEREO',
+                    u'SOHO': u'SOHO',
+                    u'Spacewatch': u'Spacewatch',
+                    u'SOLWIND': u'Solwind',
+                    u'Shoemaker': u'Шумейкер',
+                    u'NEAT': u'NEAT',
+                    u'McNaught': u'Макнот',
+                    u'Christensen': u'Кристенсен',
+                    u'Barnard': u'Барнард',
+                    u'LONEOS': u'LONEOS',
+                    u'Lovejoy': u'Лавджой',
+                    u'Machholz': u'Макхольц',
+                    u'Mechain': u'Мешен',
+                    u'SMM': u'SolarMax',
+                    u'Boattini': u'Боаттини',
+                    u'Bradfield': u'Брэдфилд',
+                    u'Mueller': u'Мюллер',
+                }
+                if name in tr_name:
+                    return u'{} ({})'.format(code, tr_name[name])
+                else:
+                    print('unknown: %s' % name)
+
+        return None
+
+    def process (self, msg, cat):
+        new_msgstr = self.translate(msg)
+        if new_msgstr is not None and (msg.fuzzy or msg.msgstr[0] != new_msgstr):
+            msg.msgstr[0] = new_msgstr
+            msg.unfuzzy()
+            report_msg_content(msg, cat)
+
+    def finalize (self):
+        ""
